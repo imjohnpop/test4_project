34078dafb85643b5c3e339ce7f07d49079699fa6
imjohnpop <poprocsi.j@gmail.com>
2017-11-01 09:44:03 +0100

    created models/migrations/controllers

---
+++ database/migrations/2017_11_01_083515_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id');
+            $table->string('title');
+            $table->integer('published_at');
+            $table->date('finished_reading_at');
+            $table->text('my_review');
+            $table->integer('my_rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
